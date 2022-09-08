c5a794ea74c40da51f6e75669420c2edc0b66427
Akash Singh <50767686+manduakash@users.noreply.github.com>
2022-09-08 17:43:28 +0530

    Update Question_no6.java

--- Question_no6.java
+++ Question_no6.java
@@ -5,7 +5,7 @@
 
 // Q.6 - Write a java class to accept five string name.
 
-public class switchcase {   // class
+public class Question_no6 {   // class
 	enum names{
 		arnab, sayan, sankha, akash, owashim
 	}
