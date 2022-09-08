248b79796c8dd8e7911010e5c1deadd23a603429
Akash Singh <50767686+manduakash@users.noreply.github.com>
2022-09-08 17:39:12 +0530

    Create Question_no6.java

---
+++ Question_no6.java
@@ -0,0 +1,38 @@
+
+/**
+ * @author Akash Singh
+**/
+
+// Q.6 - Write a java class to accept five string name.
+
+public class switchcase {   // class
+	enum names{
+		arnab, sayan, sankha, akash, owashim
+	}
+	public static void main(String[] args) {   // main method
+		
+		names n= names.sayan;       // read the user input
+		
+		switch(n) {    // switch case
+		case arnab : System.out.println("Switch Case is easy for Arnab.");
+		break;
+		
+		case akash : System.out.println("Switch Case is easy for Akash.");
+		break;
+		
+		case sankha : System.out.println("Switch Case is easy for sankha.");
+		break;
+		
+		case sayan : System.out.println("Switch Case is easy for Sayan.");
+		break;
+		
+		case owashim : System.out.println("Switch Case is easy for owashim.");
+		break;
+		
+		default: System.out.println("There is no one.");
+		
+		}
+
+	}
+
+}
