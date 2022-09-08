7f1338a648e754df9ee0e66ad1d511351c4bef4f
Akash Singh <50767686+manduakash@users.noreply.github.com>
2022-09-08 17:41:33 +0530

    Create Question_no4.java

---
+++ Question_no4.java
@@ -0,0 +1,20 @@
+/**
+ * @author Akash Singh
+**/
+
+// Q.5 - Write a java class to create an array of ten integers than print each of the                    
+//integers value using a for each loop.
+
+public class Question_no4 {
+
+	public static void main(String[] args) {        // main method
+		int[] arr = {78,81,91,85,65,85,74,96,69,80};   // array input
+		
+		for(int a:arr) {  // using for each loop
+			System.out.print(" "+a);   // print output
+		}
+		
+
+	}
+
+}
