f21967323d5ea11251c87d6eb2292f8a70923f20
Akash Singh <50767686+manduakash@users.noreply.github.com>
2022-09-09 17:04:20 +0530

    Update Question_no1.java

--- Question_no1.java
+++ Question_no1.java
@@ -1,50 +1,41 @@
-//@Author Akash Singh
-//Java Program to create a employee object without using 'new' keyword
+//@author Akash Singh
+public class Question_no1 {	//class
 
-class Employee{
-	
-	//using static to access these without creating any objects 
-	 static int id;
-	 static String name;
-	
-	 static void displayEmployee() {
-		 System.out.println("Employed id and name = " + id + " "+ name );
-	 }
-	
-}
-
-public class Question_no1 {
-	
-	//main method
-	public static void main(String[] args) {
+	public static void main(String[] args){	//main method
+		try{//creating objects without new keyword
 		
-		//passing the employee details without instantiating any object
+		//creating objects using newInstance method
+		AnotherClass o1 = AnotherClass.class.newInstance();
+		AnotherClass o2 = AnotherClass.class.newInstance();
+		AnotherClass o3 = AnotherClass.class.newInstance();
+		AnotherClass o4 = AnotherClass.class.newInstance();
+		AnotherClass o5 = AnotherClass.class.newInstance();
 		
-		//emp1
-		Employee.id=101;
-		Employee.name="Akash";
-		Employee.displayEmployee();//output
-	
-		//emp2
-		Employee.id=102;
-		Employee.name="Sayan";
-		Employee.displayEmployee();//output
+		//initializing data members of objects
+		o1.name="akash";
+		o1.age=23;
+		o2.name="sayan";
+		o2.age=24;
+		o3.name="sankha";
+		o3.age=25;
+		o4.name="owashim";
+		o4.age=26;
+		o5.name="arnab";
+		o5.age=27;
 		
-		//emp3
-		Employee.id=103;
-		Employee.name="Sagnik";
-		Employee.displayEmployee();//output
-		
-		//emp4
-		Employee.id=104;
-		Employee.name="Mrinal";
-		Employee.displayEmployee();//output
-		
-		//emp5
-		Employee.id=105;
-		Employee.name="Ankit";
-		Employee.displayEmployee();//output
+		System.out.println("name="+o1.name+", age="+o1.age);
+		System.out.println("name="+o2.name+", age="+o2.age);
+		System.out.println("name="+o3.name+", age="+o3.age);
+		System.out.println("name="+o4.name+", age="+o4.age);
+		System.out.println("name="+o5.name+", age="+o5.age);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		
 	}
-
+}
+class AnotherClass{	//another class
+	//data members of another class
+	String name;
+	int age;
 }
